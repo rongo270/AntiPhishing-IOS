d36a08c2aec570233774a1ba342b449a916476cc
Rongo <rongo123321@gmail.com>
2026-06-10 18:44:01 +0300

    Initial Commit

---
+++ AntiPhishing/AntiPhishingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntiPhishingApp.swift
+//  AntiPhishing
+//
+//  Created by rongo on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AntiPhishingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
